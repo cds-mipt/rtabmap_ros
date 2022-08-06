5d4e26149824ac98b909026a23a4cd843c089ad3
andrey1908 <andrey.krishtopik@mail.ru>
2022-08-06 16:12:53 +0300

    rename some files

--- src/rviz/ColoredOccupancyGridDisplay.cpp
+++ src/rviz/ColoredOccupancyGridDisplay.cpp
@@ -28,7 +28,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-#include "ColoredOccupancyGrid.h"
+#include "ColoredOccupancyGridDisplay.h"
 
 #include <ros/ros.h>
 
